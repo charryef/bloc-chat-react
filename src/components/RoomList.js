52a642981d21c2a55e4ce0646ff4905b4cadbde2
Charity Fuller <charryef@gmail.com>
2018-03-21 23:19:22 -0700

    Created chat rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 
-//create a RoomList component, which will be rendered from App.js,
-//and firebase will be passed down to it as a prop
+
 class RoomList extends Component {
   constructor(props) {
     super(props);
@@ -11,7 +10,6 @@ class RoomList extends Component {
       newRoomName: ''
     };
 
-    //store a Firebase reference to the rooms path onto the this keyword.
     this.roomsRef = this.props.firebase.database().ref('rooms');
 
   }
